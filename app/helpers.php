1f270af48c949987241112f0d14938a8c6748429
2284624967 <2284624967@qq.com>
2021-03-29 11:31:32 +0000

    分类模型和数据

--- app/helpers.php
+++ app/helpers.php
@@ -1,5 +1,6 @@
 <?php
 
+//此方法会将当前请求的路由名称转换为 CSS 类名
 function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +21,8 @@ Route::get('/', 'PagesController@root')->name('root');
 Auth::routes(['verify' => true]);
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
+
 
 
 
