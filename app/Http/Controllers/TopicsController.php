7dfbab16b730874ec9ac1da8c714c1d590d0fa1a
2284624967 <2284624967@qq.com>
2021-03-31 07:58:11 +0000

    分类话题列表

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -17,6 +17,7 @@ class TopicsController extends Controller
 	public function index()
 	{
 		$topics = Topic::with('user', 'category')->paginate();
+
 		return view('topics.index', compact('topics'));
 	}
 
