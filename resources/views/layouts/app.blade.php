097bbd7243db8ff577ac14e5ddf638ce73e3d426
2284624967 <2284624967@qq.com>
2021-05-20 11:52:12 +0000

    站点权限部署

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,6 +33,11 @@
     @include('layouts._footer')
 </div>
 
+<!-- 加入suuosu -->
+@if (app()->isLocal())
+  @include('sudosu::user-selector')
+@endif
+
 <!-- Scripts -->
 <script src="{{ mix('js/app.js') }}"></script>
 @yield('scripts')
