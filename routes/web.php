a42c9ba170c69259209382a73470e002cdf6b1bc
2284624967 <2284624967@qq.com>
2021-05-24 10:08:46 +0000

    管理后台权限

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,8 @@ Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]
 
 Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
 
+Route::get('permission-denied', 'PagesController@permissionDenied')->name('permission-denied');
+
 Route::get('test/{topic}', function (Topic $topic){
     dump($topic->replies);
     foreach($topic->replies as $reply)
@@ -55,49 +57,7 @@ Route::get('test', function (){
     }
 });
 
-class Foo
-{
-    protected $item = [];
-    public function __construct()
-    {
-        $this->item[0] = new Bar;
-        $this->item[1] = new Bar;
-    }
-}
 
-class Bar
-{
-    protected $a = 1;
-    protected $b = 2;
-    protected $item = array(0=>'a',1=>'b',2=>'c',3=>'d',4=>'e',5=>'f',6=>'g',7=>'h',8=>'i',9=>'j');
-}
-
-class obj implements arrayaccess {
-    protected $container = array();
-    public function __construct() {
-        $this->container = array(
-            "one"   => 1,
-            "two"   => 2,
-            "three" => 3,
-        );
-    }
-    public function offsetSet($offset, $value) {
-        if (is_null($offset)) {
-            $this->container[] = $value;
-        } else {
-            $this->container[$offset] = $value;
-        }
-    }
-    public function offsetExists($offset) {
-        return isset($this->container[$offset]);
-    }
-    public function offsetUnset($offset) {
-        unset($this->container[$offset]);
-    }
-    public function offsetGet($offset) {
-        return isset($this->container[$offset]) ? $this->container[$offset] : null;
-    }
-}
 
 
 
